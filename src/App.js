470bb89da55b985789630719a530d72b34314d57
atifhasans <atifhasansiddiqi@gmail.com>
2024-06-02 15:46:03 +0500

    Clicker App

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import {useState} from 'react'
+import Header from './components/Header'
+import Clicker from './components/Clicker'
+import './App.css';
+
+export default function App() {
+  const [title] = useState("Clicker");
+  return (
+    <div className="App">
+      <Header title={title} />
+      <Clicker />
+    </div>
+  )
+}
+
